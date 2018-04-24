c28fd58f12ee9930398f6996ffc4c43e4bc98951
humbylcoin <humbylcoin@gmail.com>
2018-04-23 21:33:17 -0700

    add test private key

--- src/node-app.js
+++ src/node-app.js
@@ -3,6 +3,9 @@
 var Web3 = require('../node_modules/web3/index.js');
 var web3 = new Web3();
 
+// test private key (address = 0x36Ca37CDEa2D1f962394f1CaAF04fe68AEab16a9)
+var privateKey = 'cd7611709a5d2f6bed6c191d7e34ea196317115da86034e38028ac0bb66e56c2'
+
 //https://api.myetherapi.com/rop
 web3.setProvider(new web3.providers.HttpProvider('https://api.myetherapi.com/eth'));
 
